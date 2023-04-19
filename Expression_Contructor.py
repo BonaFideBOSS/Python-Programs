3447122c28a344e9fea3f38b942f608cf3253ec2
Amaan Al Mir <amaanalmir@gmail.com>
2023-04-19 15:55:10 -0400

    Add files via upload

--- Expression_Contructor.py
+++ Expression_Contructor.py
@@ -1,5 +1,4 @@
 import itertools
-import math
 
 def find_expression(numbers, target, use_all=False):
     combinations = []
@@ -32,9 +31,42 @@ def find_expression(numbers, target, use_all=False):
                 closest = (expression,value)
     return closest
 
-nums = [10,5,2,10]
+O = {'+':7, '-':3, '*':5, '/':1}
+P = {'+':lambda x,y:x+y,
+     '-':lambda x,y:x-y,
+     '*':lambda x,y:x*y,
+     '/':lambda x,y:x/y}
+
+def postfix_search(rest, stk, N):
+    if len(stk) >= 2:
+        y = (v2, r2) = stk.pop()
+        x = (v1, r1) = stk.pop()
+        for opr in O:
+            if O[opr] > 0 and not (opr == '/' and v2 == 0):
+                stk += [(P[opr](v1, v2), '('+r1+opr+r2+')')]
+                O[opr] -= 1
+                if postfix_search(rest, stk,N): return 1
+                O[opr] += 1
+                stk.pop()
+        stk += [x, y]
+    elif not rest:
+        v, r = stk[0]
+        if v == N: print(f"{r} = {N}")
+        return v == N
+    for x in list(rest):
+        rest.remove(x)
+        stk += [x]
+        if postfix_search(rest, stk,N):
+            return True
+        stk.pop()
+        rest += [x]
+
+nums = [10,2,10]
 target = 100
 use_all = False
+L = nums
+N = target
 
+postfix_search(list(zip(L, map(str, L))), [], N)
 result = find_expression(nums, target, use_all)
-print(f"Result: {result[0]} = {result[1]}")
+print(f"{result[0]} = {result[1]}")
