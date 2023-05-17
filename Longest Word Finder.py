831faab4eb61850a53b931fb96e96e07c4466131
BONAFIDE BOSS <amaanalmir@gmail.com>
2023-05-17 16:58:45 -0400

    update

---
+++ Longest Word Finder.py
@@ -0,0 +1,81 @@
+import csv
+import itertools
+import time
+
+
+def csv_to_dict(filename):
+    result_dict = {}
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            word, definition = row
+            result_dict[word] = definition
+    return result_dict
+
+
+# Read Dictionary
+print("Reading dictionary...")
+filename = "dictionary.csv"
+dictionary = csv_to_dict(filename)
+print("Reading dictionary completed!")
+
+
+def possible_words(letters):
+    possible_words = set()
+    letter_count = len(letters)
+    letters = sorted(letters)
+    while letter_count > 0:
+        for word_combination in itertools.permutations(letters, letter_count):
+            word_combination = "".join(word_combination)
+            if word_combination in dictionary:
+                possible_words.add(word_combination)
+        letter_count -= 1
+    return possible_words
+
+
+def longest_words(letters):
+    print("Processing...")
+    words = possible_words(letters.lower())
+    word_count = len(words)
+
+    if word_count > 0:
+        words = sorted(words, key=len)
+        longest_word = words[-1]
+        longest_len = len(longest_word)
+        words.remove(longest_word)
+
+        same_len_words = [item for item in words if len(item) == longest_len]
+        print("Total number of possible words: ", word_count)
+        print("Longest word length: ", len(longest_word))
+        print("Longest Word: ", longest_word)
+        print("Definition: ", dictionary[longest_word])
+        if same_len_words:
+            print(f"Other {longest_len} letter words")
+            for word in same_len_words:
+                print(f"{word}: {dictionary[word]}")
+    else:
+        print("No possible word found in dictionary.")
+
+
+def program():
+    start = time.time()
+    letters = input("\nEnter letters: ")
+    longest_words(letters)
+    end = time.time()
+    print("Processing completed!")
+    print(f"Time taken: {end-start} seconds ")
+    again()
+
+
+def again():
+    check_again = input("\nDo you want to run again? [Y/N]")
+
+    if check_again.upper() == "Y":
+        program()
+    elif check_again.upper() == "N":
+        print("See you later.")
+    else:
+        again()
+
+
+program()
