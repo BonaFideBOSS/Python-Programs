73262ed69d5fede0149043d0fdd4a3742f502633
BONAFIDE BOSS <amaanalmir@gmail.com>
2023-08-21 07:38:26 +0300

    Update add views.py

--- add views.py
+++ add views.py
@@ -1,15 +1,38 @@
 from selenium import webdriver
 import time
-number_of_drivers = int(input("Views : " ))
-url = input("URL : " )
-drivers =[]
-wait = 3
-if "twitter.com" in url:
+from concurrent.futures import ThreadPoolExecutor
+
+
+def wait_for(url):
     wait = 5
-elif "youtube.com" in url:
-    wait = 30
-for i in range(number_of_drivers):
-    drivers.append(webdriver.Chrome(executable_path = "C:\Program Files\Google\Chrome\chromedriver.exe"))
-    drivers[i].get(url)
-    time.sleep(wait)
-    drivers[i].quit()
+    if "youtube.com" in url:
+        wait = 30
+    return wait
+
+
+def add_views(url):
+    try:
+        webdriver_path = "C:\Program Files\Google\Chrome\chromedriver.exe"
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        browser = webdriver.Chrome(executable_path=webdriver_path, options=options)
+        browser.get(url)
+        time.sleep(wait_for(url))
+        browser.quit()
+    except:
+        pass
+
+
+def thread_pool(url, views):
+    urls = [url] * views
+    with ThreadPoolExecutor() as thread:
+        thread.map(add_views, urls)
+
+
+def get_user_input():
+    url = input("URL : ")
+    views = int(input("Views : "))
+    thread_pool(url, views)
+
+
+get_user_input()
